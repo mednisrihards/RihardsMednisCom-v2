1b748a18cbb859cb1ff6eb1feab268853eacb408
Rihards Mednis <mednisrihards@gmail.com>
2021-01-15 21:29:06 +0200

    Fixed email sending

--- send-email.php
+++ send-email.php
@@ -25,15 +25,13 @@ if(isset($_POST["email"]) && isset($_POST["message"])){
     $mail -> isSMTP();
     $mail -> Host = "smtp.gmail.com";
     $mail -> SMTPAuth = true;
-    // $mail -> Username = $_ENV['USERNAME'];
-    // $mail -> Password = $_ENV['PASSWORD'];
-    $mail -> Username = getenv('USERNAME');
-    $mail -> Password = getenv('PASSWORD');
+    $mail -> Username = $_ENV['USERNAME'];
+    $mail -> Password = $_ENV['PASSWORD'];
     $mail -> Port = 465;
     $mail -> SMTPSecure = "ssl";
 
     $mail -> isHTML(true);
-    $mail -> setFrom("mednisrihardscom@gmail.com", $email);
+    $mail -> setFrom($_ENV['USERNAME'], $email);
     $mail -> addAddress("mednis.rihards@gmail.com");
     $mail -> Subject = "Mail from rihardsmednis.com";
     $mail -> Body = $body;
